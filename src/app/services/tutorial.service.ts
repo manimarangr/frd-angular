f06708829cc22e077226466f46979370d5067d33
manimarangr <maran.php@gmail.com>
2026-05-19 22:25:16 +0530

    env changed

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 
 const baseUrl = environment.apiUrl;
 
